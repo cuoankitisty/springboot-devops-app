e6457aa5eb6692a19586a23d3658c263a08cee80
Ankit Jha <kumarjhaankit0@gmail.com>
2025-10-17 14:55:01 +0530

    Added the setup

---
+++ src/main/java/com/demo/springbootdevopsapp/SpringbootDevopsAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootdevopsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevopsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDevopsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/springbootdevopsapp/SpringbootDevopsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootdevopsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDevopsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
